5b6295bb56734f3319e5c5a002378354031ab9f2
Jeremiah Vaughan <46267375+JeremiahVaughan@users.noreply.github.com>
2025-02-01 23:28:24 -0600

    was experimenting with css and decided to fix the hot reloader
    Two problems, the debounce wasn't working correctly and the request
    context wasn't being cleanup each time the client would reconnect which
    would eventually lead to a program crash because multple threads would
    be trying to update the templates map at the same time.

--- HOT_reload.go
+++ HOT_reload.go
@@ -12,56 +12,110 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
-type ThreadSafeStringBuilder struct {
-	mu sync.Mutex
-	b  strings.Builder
+type Debouncer struct {
+	delay   time.Duration
+	timer   *time.Timer
+	mu      sync.Mutex
+	started bool
 }
 
-var UiFilesBeChangin chan fsnotify.Event = make(chan fsnotify.Event)
+func NewDebouncer(delay time.Duration) *Debouncer {
+	return &Debouncer{
+		delay: delay,
+	}
+}
+
+func (d *Debouncer) Debounce(f func()) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	// If a timer is already running, stop it
+	if d.timer != nil {
+		d.timer.Stop()
+	}
+
+	// Start a new timer that will call the function after the delay
+	d.timer = time.AfterFunc(d.delay, func() {
+		f()
+		d.mu.Lock()
+		d.timer = nil // Allow the next function call to use a new timer
+		d.mu.Unlock()
+	})
+}
+
+const fileEventsBufferSize = 30
+
+var UiFilesBeChangin chan fsnotify.Event = make(chan fsnotify.Event, fileEventsBufferSize)
+var UiFilesBeChanginTimes chan int64 = make(chan int64, fileEventsBufferSize)
 
 func handleHotreload(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	// watch for events and enrich with current time
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case event := <-UiFilesBeChangin:
+				currentTime := time.Now().UnixMilli()
+				if isChangedEvent(event) {
+					log.Printf(
+						"file change event detected. File: %s. Operation: %s. Time: %d",
+						event.Name,
+						event.Op.String(),
+						currentTime,
+					)
+					log.Printf("starting channel at %d", time.Now().UnixMilli())
+					UiFilesBeChanginTimes <- currentTime
+					log.Printf("draining channel at %d", time.Now().UnixMilli())
+				} else {
+					log.Printf(
+						"file non change event detected. File: %s. Operation: %s. Time: %d",
+						event.Name,
+						event.Op.String(),
+						currentTime,
+					)
+				}
+			}
+		}
+	}()
+
 	sendHeaders(w)
 	i := 0
 	retrySleep := 250 * time.Millisecond
-	var lastEventTime int64
-	var events ThreadSafeStringBuilder
+	// wait till all file operations have settled, this ensures the
+	// files are in the desired state when we parse them
+	// debounce 50 miliseconds, as it seems file events are around 6 miliseconds apart and there are 3-4 each time
+	db := NewDebouncer(50 * time.Millisecond)
+	sb := strings.Builder{}
 	for {
-		event := <-UiFilesBeChangin
-		// debounce 5 miliseconds
-		debounceTimeMilli := int64(5)
-		if !isChangedEvent(event) {
-			continue
+		select {
+		case <-ctx.Done():
+			return
+		case eventTime := <-UiFilesBeChanginTimes:
+			db.Debounce(func() {
+				_, err := sb.WriteString(fmt.Sprintf("name=%d@time=%d", eventTime, time.Now().UnixMilli()))
+				if err != nil {
+					log.Fatalf("error, when writing event to string builder. Error: %v", err)
+				}
+
+				err2 := parseDevTemplates()
+				if err2 != nil {
+					err2 = fmt.Errorf("error, when parseDevTemplates() for handleHotreload(). Error: %v", err2)
+					// http.Error(w, err2.Error(), http.StatusInternalServerError) // not returning the error because in golang you can't set the status code more than once in a single call.
+					// And this is a long running call so it is likely to happen more than once
+					log.Fatal(err2.Error())
+				}
+				evt := fmt.Sprintf(
+					"id:%X\nretry:%d\ndata:%s\n\n",
+					i, retrySleep, sb.String(),
+				)
+				w.Write([]byte(evt))
+				log.Printf("message sent: %d", eventTime)
+				w.(http.Flusher).Flush()
+				i++
+			})
 		}
-		events.mu.Lock()
-		_, err := events.b.WriteString(fmt.Sprintf("name=%s@time=%d", event.Name, time.Now().UnixMilli()))
-		if err != nil {
-			log.Fatalf("error, when writing event to string builder. Error: %v", err)
-		}
-		events.mu.Unlock()
-		if lastEventTime != 0 && lastEventTime+debounceTimeMilli < time.Now().UnixMilli() {
-			continue
-		}
-		lastEventTime = time.Now().UnixMilli()
-		go func(index int) {
-			// wait till all file operations have settled, this ensures the files are in the desired state when we parse them
-			time.Sleep(time.Duration(debounceTimeMilli) * time.Millisecond)
-			err2 := parseDevTemplates()
-			if err2 != nil {
-				err2 = fmt.Errorf("error, when parseDevTemplates() for handleHotreload(). Error: %v", err2)
-				http.Error(w, err2.Error(), http.StatusInternalServerError)
-				log.Fatal(err2.Error())
-			}
-			events.mu.Lock()
-			evt := fmt.Sprintf(
-				"id:%X\nretry:%d\ndata:%s\n\n",
-				index, retrySleep, events.b.String(),
-			)
-			events.b.Reset()
-			events.mu.Unlock()
-			w.Write([]byte(evt))
-			w.(http.Flusher).Flush()
-			index++
-		}(i)
 	}
 }
 
